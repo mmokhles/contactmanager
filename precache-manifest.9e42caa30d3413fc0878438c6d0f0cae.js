0ddec50e4c0fe5de013669975750bc1d50b586f8
Mohammed Mokhles <m.mokhles.hammad@gmail.com>
2020-05-10 00:02:07 +0200

    Updates

---
+++ precache-manifest.9e42caa30d3413fc0878438c6d0f0cae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48aa0b148003fa896f47fc06cebe15d9",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "2faddfdce294206e82d9",
+    "url": "/contactmanager/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "f1cd3625050bed266296",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "2faddfdce294206e82d9",
+    "url": "/contactmanager/static/js/2.82151bc3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.82151bc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1cd3625050bed266296",
+    "url": "/contactmanager/static/js/main.21668d5d.chunk.js"
+  },
+  {
+    "revision": "3959f31844f85f026a86",
+    "url": "/contactmanager/static/js/runtime-main.711199b3.js"
+  }
+]);
